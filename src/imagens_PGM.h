ec113ad40d848834769363e62d6b67821e9f5af3
GGuedesAB <gustavoguedesab@gmail.com>
2019-03-28 12:29:14 -0300

    Fixing some minor issues and sending documentation file.

--- src/imagens_PGM.h
+++ src/imagens_PGM.h
@@ -13,22 +13,105 @@ typedef struct
     unsigned char **imagem; //Variavel para armazenar os pixels da imagem (matriz)
 }PGM;
 
-PGM *LerPGM(char *entrada);
+/*---------------------------------------------------------------------------
+Protótipo: char** CriaKernel().
+Função: Alocar dinâmicamente e atribuir valores da matriz "kernel de Prewitt" a
+um char**.
+Entrada: Vazio.
+Saída: Matriz 3x3 do tipo kernel de Prewitt.
+-----------------------------------------------------------------------------*/
+char**CriaKernel();
 
-PGM *cria_PGM(int l, int c, int maxim);
 
-PGM* expande_imagem(PGM *imagem);
 
-PGM* Copia_PGM (PGM* original);
+/*---------------------------------------------------------------------------
+Protótipo: char** DesalocaKernel(char **kernel_alocado).
+Função: Desalocar a matriz "kernel de Prewitt" previamente alocada.
+Entrada: A matriz "kernel de Prewitt" alocada.
+Saída: Variável desalocada e apontando para NULL.
+-----------------------------------------------------------------------------*/
+char** DesalocaKernel(char **kernel_alocado);
 
+
+
+/*---------------------------------------------------------------------------
+Protótipo: PGM* LerPGM(char *entrada).
+Função: Ler a imagem de um arquivo "*.pgm" e a retornar para um ponteiro de PGM.
+Entrada: O nome do arquivo a ser lido.
+Saída: A imagem do tipo pgm lida do arquivo.
+-----------------------------------------------------------------------------*/
+PGM* LerPGM(char *entrada);
+
+
+
+
+/*---------------------------------------------------------------------------
+Protótipo: PGM* CriaPGM(int l, int c, int maxim).
+Função: Alocar um ponteiro do tipo PGM e seu elemento imagem segundo o numero
+de linhas, numero de colunas e valor maximo de um pixel.
+Entrada: Numero de linhas, numero de colunas, valor maximo do pixel.
+Saída: Um ponteiro do tipo PGM e seu elemento imagem devidamente alocados.
+-----------------------------------------------------------------------------*/
+PGM* CriaPGM(int l, int c, int maxim);
+
+
+
+
+/*---------------------------------------------------------------------------
+Protótipo: PGM* DesalocaPGM (PGM *alocado).
+Função: Desalocar um ponteiro do tipo PGM.
+Entrada: Um ponteiro do tipo PGM que está alocado.
+Saída: Um ponteiro do tipo PGM e seu elemento imagem devidamente desalocados e
+apontando para NULL.
+-----------------------------------------------------------------------------*/
+PGM* DesalocaPGM (PGM *alocado);
+
+
+
+
+/*---------------------------------------------------------------------------
+Protótipo: PGM* ExpandeImagem(PGM* imagem).
+Função: Expandir o elemento imagem contido em um ponteiro do tipo PGM,
+preparando-o para a convolução.
+Entrada: Um ponteiro do tipo PGM.
+Saída: O elemento imagem do tipo PGM expandido.
+-----------------------------------------------------------------------------*/
+PGM* ExpandeImagem(PGM *imagem);
+
+
+
+
+/*---------------------------------------------------------------------------
+Protótipo: PGM* AlocaPGM(PGM* original).
+Função: Alocar um ponteiro do tipo PGM segundo outro ponteiro do tipo PGM.
+Entrada: Um ponteiro do tipo PGM.
+Saída: Um ponteiro do tipo PGM e seu elemento imagem devidamente alocados.
+-----------------------------------------------------------------------------*/
+PGM* AlocaPGM (PGM* original);
+
+
+
+
+/*---------------------------------------------------------------------------
+Protótipo: void SalvarPGM(PGM *img, char *saida).
+Função: Salvar em um arquivo "*.pgm" um tipo PGM.
+Entrada: Um ponteiro do tipo PGM e o nome do arquivo que será salvado.
+Saída: Um arquivo "*.pgm" com o conteúdo de um tipo PGM.
+-----------------------------------------------------------------------------*/
 void SalvarPGM(PGM *img, char *saida);
 
+
+
+
+/*---------------------------------------------------------------------------
+Protótipo: PGM* Convolucao(PGM *img, char **kernel, PGM *saida).
+Função: Realizar a operação de convolução entre o elemento imagem de um tipo
+PGM e a matriz kernel e salvar o resultado em um outro tipo PGM.
+Entrada: Um ponteiro do tipo PGM , uma matriz kernel e outro ponteiro do tipo
+PGM.
+Saída: O resultado da operação de convolução entre o elemento imagem do ponteiro
+do tipo PGM "img" e a matriz "kernel".
+-----------------------------------------------------------------------------*/
 void Convolucao(PGM *img, char **kernel, PGM *saida);
-
-void desaloca_PGM (PGM* alocado);
-
-void desaloca_kernel(char **kernel_alocado);
-
-char** cria_kernel();
 
 #endif // IMAGENS_PGM_H_INCLUDED
--- src/main.c
+++ src/main.c
@@ -1,25 +1,31 @@
 #include "imagens_PGM.h"
-#define DEFAULT_ARG_NUMBER 3
 
 int main(int argc, char *argv[])
 {
-	for(int i = DEFAULT_ARG_NUMBER-1; i > 0; i--)
-	{
-		if(NULL == argv[i]){
-			printf("Missing arguments.\nCorrect example: \"./a.out source.pgm destination.pgm\" \n");
-			exit(0);
-		}
-	}
-    PGM *minha_img;
-    PGM *saida;
-    char **kernel;
-    kernel = cria_kernel();
-    minha_img = LerPGM(argv[argc-2]);
-    saida = Copia_PGM(minha_img);
+    for(int i = DEFAULT_ARG_NUMBER-1; i > 0; i--)
+    {
+        if(NULL == argv[i]){
+            printf("Missing arguments.\nCorrect example: \"./a.out source.pgm destination.pgm\" \n");
+            exit(0);
+        }
+    }
+    char **kernel = CriaKernel();
+    PGM *minha_img = LerPGM(argv[1]);
+    PGM *saida = AlocaPGM(minha_img);
+    minha_img = ExpandeImagem(minha_img);
     Convolucao(minha_img, kernel, saida);
-    SalvarPGM(saida, argv[argc-1]);
-    desaloca_PGM(minha_img);
-    desaloca_PGM(saida);
-    desaloca_kernel(kernel);
+    SalvarPGM(saida, argv[2]);
+    saida = DesalocaPGM(saida);
+    minha_img = DesalocaPGM(minha_img);
+    kernel = DesalocaKernel(kernel);
+    if (NULL == saida && NULL == minha_img && NULL == kernel)
+    {
+        printf("\n\n**Convolucao feita com sucesso.**\n\n");
+    }
+    else
+    {
+        printf("\n\nOcorreu um erro ao desalocar os espaços de memoria utilizados.\n");
+        exit(1);
+    }
     return 0;
 }
